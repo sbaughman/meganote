1914092e1f46d0e53b3357a4bd4c4cff86bcf754
Seth Baughman <sbaughman1@me.com>
2016-07-07 14:50:00 -0400

    Add API_BASE constant

--- app/core/constants.js
+++ app/core/constants.js
@@ -1,5 +1,6 @@
 {
   angular
     .module('meganote.core')
-    .constant('NOTES_URL', 'http://localhost:3030/notes/');
+    .constant('NOTES_URL', 'http://localhost:3030/notes/')
+    .constant('API_BASE', 'http://localhost:3030/');
 }
