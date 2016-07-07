a855e53b4714eba2657e4ef0b11ccc9937eb135e
Seth Baughman <sbaughman1@me.com>
2016-07-07 11:16:37 -0400

    add directive definition and controller.  Add markup for sign-up.html

--- app/sign-up/sign-up.directive.js
+++ app/sign-up/sign-up.directive.js
@@ -1,9 +1,23 @@
 {
   angular
     .module('meganote.signUp')
-    .directive('sbSignUp', sbSignUp);
+    .directive('sbSignUp', () => {
 
-  function sbSignUp() {
-    
-  }
+      class SignUpController {
+        constructor() {
+          this.user = {};
+        }
+        submit() {
+          console.log(this.user);
+        }
+      }
+
+      return {
+        templateUrl: '/sign-up/sign-up.html',
+        controller: SignUpController,
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {},
+      };
+    });
 }
--- app/users/config.route.js
+++ app/users/config.route.js
@@ -7,7 +7,7 @@
 
   function configFunction($stateProvider) {
     $stateProvider
-      .$state('sign-up', {
+      .state('sign-up', {
         url: '/sign-up',
         template: '<sb-sign-up></sb-sign-up>'
       });
