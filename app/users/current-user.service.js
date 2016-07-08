55ee5a886c881ca3639e209aa78f2378d62467fd
Seth Baughman <sbaughman1@me.com>
2016-07-08 11:35:21 -0400

    Add current-user service

---
+++ app/users/current-user.service.js
@@ -0,0 +1,26 @@
+{
+  angular
+    .module('meganote.users')
+    .service('CurrentUser', ['$window', $window => {
+
+      class CurrentUser {
+        constructor() {
+          this.user = JSON.parse($window.localStorage.getItem('currentUser'));
+        }
+        get() {
+          return this.user || {};
+        }
+        set(user) {
+          this.user = user;
+          $window.localStorage.setItem('currentUser', JSON.stringify(this.user));
+        }
+        clear() {
+          this.user = undefined;
+          $window.localStorage.removeItem('currentUser');
+        }
+      }
+
+      return new CurrentUser();
+
+    }]);
+}
