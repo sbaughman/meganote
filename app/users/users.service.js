e0c63e24b99f40219df807a097a76e3175459453
Seth Baughman <sbaughman1@me.com>
2016-07-08 11:29:14 -0400

    add authToken service and use it in usersService

--- app/users/users.service.js
+++ app/users/users.service.js
@@ -1,17 +1,20 @@
 {
   angular
     .module('meganote.users')
-    .service('UsersService', ['$http', 'API_BASE', ($http, API_BASE) => {
+    .service('UsersService', ['$http', 'API_BASE', 'AuthToken', ($http, API_BASE, AuthToken) => {
 
       const USERS_URL = `${API_BASE}users/`;
 
       class UsersService {
+
+        // Sign up
         create(user) {
           return $http.post(USERS_URL, {
             user
           })
-            .then((returned_user) => {
-              console.log(returned_user);
+            .then((res) => {
+              AuthToken.set(res.data.authToken);
+              console.log(`Gotten: ${AuthToken.get()}`);
             })
             .catch((error) => {
               console.log(error);
