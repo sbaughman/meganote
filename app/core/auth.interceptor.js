16662dd30ae40d71756f752d61982137081c66ea
Seth Baughman <sbaughman1@me.com>
2016-07-12 09:47:25 -0400

    Add AuthInterceptor to add authorization header to requests to our API

---
+++ app/core/auth.interceptor.js
@@ -0,0 +1,26 @@
+{
+  angular
+    .module('meganote.core')
+    .factory('AuthInterceptor', AuthInterceptor)
+    .config(interceptorConfig);
+
+  AuthInterceptor.$inject = ['AuthToken', 'API_BASE'];
+
+  function AuthInterceptor(AuthToken, API_BASE) {
+    return {
+      request(req) {
+        const token = AuthToken.get();
+        if (token && req.url.includes(API_BASE)) {
+          req.headers.Authorization = token;
+        }
+        return req;
+      }
+    }
+  }
+
+  interceptorConfig.$inject = ['$httpProvider'];
+
+  function interceptorConfig($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+  }
+}
